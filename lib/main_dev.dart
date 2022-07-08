ee50f77629ea7e0939c08a91e092a6b30f7aa888
hoang.nvm1112 <hoangnvm@vmodev.com>
2022-07-08 11:31:52 +0700

    test 1

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -11,6 +11,7 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 
 //sdád  nád kjasbd pull dev
+// ádsd
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
