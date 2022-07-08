e62081d05492d3cce14261afc90352f690fc1b96
hoang.nvm1112 <hoangnvm@vmodev.com>
2022-07-08 09:08:45 +0700

    add login api

--- lib/data/data_source/remote/auth_service.g.dart
+++ lib/data/data_source/remote/auth_service.g.dart
@@ -15,6 +15,23 @@ class _AuthService implements AuthService {
 
   String? baseUrl;
 
+  @override
+  Future<UserEntity> login(param) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(param.toJson());
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<UserEntity>(
+            Options(method: 'GET', headers: _headers, extra: _extra)
+                .compose(_dio.options, '/login',
+                    queryParameters: queryParameters, data: _data)
+                .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = UserEntity.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
